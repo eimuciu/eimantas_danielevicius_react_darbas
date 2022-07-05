ed7a133f807fca0a719221ec458d4291dd18d455
ed <allgoodata@outlook.com>
2022-07-05 09:27:14 +0300

    Implementing loading and empty content feature on to HomePage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,23 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { getDataFromServer } from '../../api';
 import { BigHeader } from '../atoms/Header';
 import CardsList from '../organisms/CardsList';
+import { useLoading } from '../../hooks';
 
-async function getData(setSkillsData) {
+async function getData(setSkillsData, setIsLoading) {
   const data = await getDataFromServer();
   setSkillsData(data);
+  setIsLoading(false);
 }
 
 function HomePage() {
   const [skillsData, setSkillsData] = useState([]);
+  const [isLoading, setIsLoading] = useLoading();
 
   useEffect(() => {
-    getData(setSkillsData);
-  }, []);
+    setIsLoading(true);
+    getData(setSkillsData, setIsLoading);
+  }, [setIsLoading]);
 
   return (
     <>
-      {skillsData.length === 0 ? (
+      {isLoading ? (
         <div>Waiting for content...</div>
+      ) : skillsData.length === 0 ? (
+        <div>No records added yet...</div>
       ) : (
         <>
           <BigHeader text={'Skills'} />
